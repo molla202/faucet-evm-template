1c23af3a2b601729b924efffca9377f5b7e445bb
molla202 <91562185+molla202@users.noreply.github.com>
2025-03-19 14:15:38 +0300

    Update transferCoin.ts

--- src/transferCoin.ts
+++ src/transferCoin.ts
@@ -1,26 +1,24 @@
 import wallet from "./wallet";
+import { ethers } from "ethers";
 
 type TransferCoin = {
   success: boolean;
   message: string;
 };
 
-/*
- * Transfer coin to address. This is native token ie ETH
- * @param {string} address - The address to transfer to
- */
 export default async function transferCoin(address: string): Promise<TransferCoin> {
   try {
+    const value = ethers.utils.parseUnits(process.env.VALUE, "ether");
     const transaction = await wallet.sendTransaction({
       to: address,
-      value: process.env.VALUE as string,
+      value: value.toHexString(), // Hexadecimal string olarak gönder
     });
     return {
       success: true,
       message: transaction.hash,
     };
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return {
       success: false,
       message: "Unable to Send Transaction",
